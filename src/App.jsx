ff01b00df1d267db5749ed1da0fd557b34541b82
asnganje <abd.nganje@gmail.com>
2024-01-12 22:40:16 +0300

    Apply multiple state update by watching for movies/reset action

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,16 @@
 import { useDispatch } from "react-redux";
 import SongPlayList from "./components/SongPlayList";
 import MoviePlayList from "./components/moviePlayList";
-import { resetM} from "./store/slices/movieSlice";
-import {resetS} from './store/slices/songSlice'
+import { reset} from "./store/slices/movieSlice";
 const App = () => {
 
     const dispatch = useDispatch()
     const resetHandler = () => {
-        dispatch(resetM())
-        dispatch(resetS())
+        dispatch(reset())
     }
     return(
-        <div className="flex flex-col shadow-lg bg-gray-500 rounded-md mx-[25vh] p-5 gap-5 font-mono items-center h-[90vh] m-5">
-            <button onClick={resetHandler} className="bg-red-400 text-2xl text-white p-2 rounded-md">Reset both playlists</button>
+        <div className="flex flex-col bg-gray-700 rounded-md mx-[25vh] p-5 gap-5 font-mono items-center h-[90vh] m-5">
+            <button onClick={resetHandler} className="bg-red-600 text-2xl text-white p-2 rounded-md">Reset both playlists</button>
             <hr />
             <MoviePlayList className = ""/>
             <hr />
