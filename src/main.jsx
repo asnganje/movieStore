a62ac41ec9cebf3f8fcbe3e01e2ca08f79770084
asnganje <abd.nganje@gmail.com>
2024-01-12 15:49:41 +0300

    add basic vite and tailwind setup

---
+++ src/main.jsx
@@ -0,0 +1,10 @@
+import './index.css'
+import {createRoot} from 'react-dom/client'
+import App from './App.jsx'
+
+const el = document.getElementById('root')
+const root = createRoot(el)
+root.render(
+  <App/>
+)
+
